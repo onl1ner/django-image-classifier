8fe98800d5ccf26c5268a831fbaf43a03a48b511
dangola02 <zhanataev.kostanay@gmail.com>
2022-03-01 21:12:32 +0600

    ready

--- imageclassifier/urls.py
+++ imageclassifier/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from imageclassifierapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.index)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
