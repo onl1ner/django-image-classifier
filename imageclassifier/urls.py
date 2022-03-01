0674416cae09500488f5437864bdda0519322719
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2022-03-02 00:50:45 +0600

    Removed unnecessary import

--- imageclassifier/urls.py
+++ imageclassifier/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
 from django.conf import settings
 from django.conf.urls.static import static
-from imageclassifierapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
