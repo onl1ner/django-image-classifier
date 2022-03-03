07acbf0faebb19ec281cab013bda735607267636
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2022-03-03 23:51:41 +0600

    Fixed fatal error on start up

--- imageclassifier/settings.py
+++ imageclassifier/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL  = '/media/'
 
+DATABASES = {}
+
 # Application definition
 
 INSTALLED_APPS = [
