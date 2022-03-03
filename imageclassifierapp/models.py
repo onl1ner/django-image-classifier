1982ef57998ef942378aa25d5edc51ec7e50c5d5
SpawNKZ <70969409+SpawNKZ@users.noreply.github.com>
2022-03-03 15:32:01 +0600

    python decouple

--- imageclassifierapp/models.py
+++ imageclassifierapp/models.py
@@ -5,4 +5,3 @@ from django.db import models
 class ImgClass(models.Model):
     img_path = models.CharField(max_length=100)
     img_classifier = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='images')
